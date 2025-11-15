03eb6f9ff9beab2ac9d1803bb6d649345373395c
wuyong <biggodw@gamil.com>
2025-11-15 15:40:45 +0800

    使用supabase获取数据

---
+++ app/components/mycarouts.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>轮播图</h1>
+    <div v-if="mycarouts.length > 0">
+        <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+            <van-swipe-item v-for="item in mycarouts" :key="item.id">{{ item.title }}</van-swipe-item>
+        </van-swipe>
+    </div>
+</template>
+<script setup>
+
+const { data: mycarouts } = await useAsyncData('mycarouts', async () => {
+    const res = await useSupabaseClient().from('carousel').select('*').eq('is_active', true).order('order', { ascending: true })
+    return res.data ?? []
+}, { default: () => [] })
+
+</script>
+<style>
+  .my-swipe .van-swipe-item {
+    color: #fff;
+    font-size: 20px;
+    line-height: 150px;
+    text-align: center;
+    background-color: #39a9ed;
+  }
+</style>
